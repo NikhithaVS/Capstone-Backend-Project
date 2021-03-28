d4c99d82e34715de72afd7b8f5624d6b142b1e33
Nikhitha V S <nikhithabharadwaj14@gmail.com>
2021-03-28 21:29:58 +0530

    Add code for payment controller

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/PaymentService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/PaymentService.java
@@ -1,4 +1,20 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
+import com.upgrad.FoodOrderingApp.service.dao.PaymentDao;
+import com.upgrad.FoodOrderingApp.service.entity.PaymentEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
+
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
 public class PaymentService {
+    @Autowired
+    PaymentDao paymentDao;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public List<PaymentEntity> getAllPaymentMethods() {
+        return paymentDao.getAllPaymentMethods();
+    }
+
 }
