7f9998c78a97239ebbf30d361b82d4730cb16ddb
Nikhitha V S <nikhithabharadwaj14@gmail.com>
2021-03-29 06:41:10 +0530

    Add code for category controller and its dependent entities

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CategoryController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CategoryController.java
@@ -1,31 +1,81 @@
 package com.upgrad.FoodOrderingApp.api.controller;
 
+import com.upgrad.FoodOrderingApp.api.model.CategoriesListResponse;
 import com.upgrad.FoodOrderingApp.api.model.CategoryDetailsResponse;
 import com.upgrad.FoodOrderingApp.api.model.CategoryListResponse;
+import com.upgrad.FoodOrderingApp.api.model.ItemList;
+import com.upgrad.FoodOrderingApp.service.businness.CategoryService;
+import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
 import com.upgrad.FoodOrderingApp.service.exception.CategoryNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/")
 public class CategoryController {
 
+  @Autowired
+  private CategoryService categoryService;
+
+  @CrossOrigin
   @RequestMapping(
-      method = RequestMethod.GET,
-      path = "/category",
-      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public ResponseEntity<CategoryListResponse> getAllCategories() {
-    return null;
+          method = RequestMethod.GET,
+          path = "/category",
+          produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<CategoriesListResponse> getAllCategoriesOrderedByName() {
+
+    List<CategoryEntity> categoryEntityList = categoryService.getAllCategoriesOrderedByName();
+
+    CategoriesListResponse categoriesListResponse = new CategoriesListResponse();
+
+    categoryEntityList.forEach(
+            category ->
+                    categoriesListResponse.addCategoriesItem(
+                            new CategoryListResponse()
+                                    .id(UUID.fromString(category.getUuid()))
+                                    .categoryName(category.getCategoryName())));
+
+    return new ResponseEntity<CategoriesListResponse>(categoriesListResponse, HttpStatus.OK);
   }
 
+  @CrossOrigin
   @RequestMapping(
-      method = RequestMethod.GET,
-      path = "/category/{category_id}",
-      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+          method = RequestMethod.GET,
+          path = "/category/{category_id}",
+          produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<CategoryDetailsResponse> getCategoryById(
-      @PathVariable("category_id") final String categoryId) throws CategoryNotFoundException {
-    return null;
+          @PathVariable("category_id") final String categoryId) throws CategoryNotFoundException {
+
+    CategoryEntity categoryEntity = categoryService.getCategoryById(categoryId);
+
+    ArrayList<ItemList> itemList = new ArrayList<>();
+
+    categoryEntity
+            .getItems()
+            .forEach(
+                    items ->
+                            itemList.add(
+                                    new ItemList()
+                                            .id(UUID.fromString(items.getUuid()))
+                                            .itemName(items.getItemName())
+                                            .itemType(ItemList.ItemTypeEnum.fromValue(items.getType().getValue()))
+                                            .price(items.getPrice())));
+
+    // Map retrieved Category Entity to Response Object List
+    CategoryDetailsResponse categoryDetailsResponse =
+            new CategoryDetailsResponse()
+                    .categoryName(categoryEntity.getCategoryName())
+                    .id(UUID.fromString(categoryEntity.getUuid()))
+                    .itemList(itemList);
+
+    return new ResponseEntity<CategoryDetailsResponse>(categoryDetailsResponse, HttpStatus.OK);
   }
 }
